315697e959ccaa2cfb07910aab69858853ee66f7
Mike Bush <mbush91@gmail.com>
2024-07-22 19:47:18 +0000

    printing stuff

--- raidCheck.py
+++ raidCheck.py
@@ -1,7 +1,22 @@
 import paho.mqtt.client as mqtt
+import subprocess
+
+perccli_cmd = ['/bin/perccli64', '/c0' ,'show','all']
 
 def main() :
   print("HI")
+
+  try :
+    result = subprocess.run(perccli_cmd, capture_output=True, text=True, check=True)
+    output = result.stdout
+    error = result.stderr
+    print("Command output:")
+    print(output)
+    print("Command error:")
+    print(error)
+  except subprocess.CalledProcessError as e:
+    print(f"Command failed with exit code {e.returncode}")
+    print(f"Error output: {e.stderr}")
 
 if __name__ == "__main__":
   main()
