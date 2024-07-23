e167b80929c633b3b6d0e69dfe4db91533115171
MIke Bush <mike.bush@valtech.com>
2024-07-23 08:15:47 -0400

    parsing disk states

--- raidCheck.py
+++ raidCheck.py
@@ -1,7 +1,37 @@
 import paho.mqtt.client as mqtt
+import re
 import subprocess
+import json
 
 perccli_cmd = ['/bin/perccli64', '/c0' ,'show','all']
+
+def parse_perccli(output) -> dict :
+
+  out = {}
+
+  pattern = re.compile(r'([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+:[0-9]+)\s+([0-9]+)\s+(DRIVE|RAID[0-5])\s+(Onln|Rbld|Optl|Dgrd|Pdgd|Offln)')
+  m = pattern.findall(output)
+
+  for disk in m :
+    k = 'DRIVE'+disk[0]
+    if k in out :
+      d = "DISK"+disk[2]
+      if d not in out[k] :
+        out[k][d] = disk[6]
+      else :
+        raise Exception("Repeated Disk!")
+    else :
+      out[k] = {
+       "DISK"+disk[2] : disk[6]
+      }
+
+  fmt = json.dumps(out,indent=2)
+  print(fmt)
+
+  return out
+
+def report_disk(states) :
+  pass
 
 def main() :
   print("HI")
